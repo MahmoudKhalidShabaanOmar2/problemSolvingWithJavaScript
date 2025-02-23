aeb1a9cdfacc2c26971592ab2265f996585dfc63
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-24 01:16:35 +0200

    Add files via upload

---
+++ problemJS1.js
@@ -0,0 +1,5 @@
+document.getElementById("welcome").innerHTML = "hello everyone! , and welcome to 1- problem solving with java script"
+window.alert("Hello Everyone , And Welcome To Problem Solving With Java Script")
+// write a java script program to get number from the user , and then display the number for the user in the console =>
+var number = window.prompt("Please Enter The Number Is = ")
+console.log("The Number Is = " , number)

c43efadef5322d0649ac8bdc056815c7ea888fef
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-02-24 02:36:13 +0200

    Add files via upload

---
+++ problemJS4.js
@@ -0,0 +1,11 @@
+document.getElementById("welcome").innerHTML = "Hello Everyone! , And Welcome To 4-Problem Solving With Java Script"
+window.alert("Hello Everyone! , And Welcome To 6-Problem Solving With Java Script")
+// write a java script program to check if the number is a positive number , or negative number =>
+var number = window.prompt("Please Enter The Number Is = ")
+console.log("The Number Is = ",number)
+if(number > 0){
+    console.log("It Is A Positive Number , Because Your Entered Number Is = ",number)
+}
+else{
+    console.log("It Is A Negative Number , Because Your Entered Number Is = ",number)
+}

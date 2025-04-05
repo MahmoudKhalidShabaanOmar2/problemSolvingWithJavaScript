e789ce5a54cfdd37c1aea75b50978c872bc7b9d1
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-05 17:55:32 +0200

    Add files via upload

---
+++ problem2/js/index.js
@@ -0,0 +1,4 @@
+// write a java script program to get name of the user print a welcome sentence in this format , hello everyone! , and welcome "userName" =>
+document.getElementById("mainHeading").innerHTML = "display hello everyone! , and welcome \"userName\""
+var userName = "mahmoud" 
+console.log("hello everyone! , and wwelcome \"",userName,"\"")

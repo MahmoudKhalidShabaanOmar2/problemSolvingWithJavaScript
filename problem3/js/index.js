67a05cf15eda8033f72a5f32a8f6f909b90d8d27
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-05 18:31:56 +0200

    Add files via upload

---
+++ problem3/js/index.js
@@ -0,0 +1,13 @@
+// write a java script program to get the full name of the user , user age , and also user gender , and then display in the console => 
+document.getElementById("mainHeading").innerHTML = "display different information of the user"
+var fristName = "mahmoud"
+console.log("the frist name is : ",fristName)
+var secondName = "khalid" 
+console.log("the second name is : ",secondName)
+var lastName = "shabaan"
+console.log("the last name is : ",lastName)
+console.log("the full name of the user is : ",fristName," ",secondName," ",lastName)
+var userAge = 22 
+console.log("the age of the user is : ",userAge," years")
+var userGender = "M"
+console.log("the gender of the user is : ",userGender)

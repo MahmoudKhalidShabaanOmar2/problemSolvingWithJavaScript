89b9eb33bab35cd32c60949911083cffe16f1665
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-05 19:22:30 +0200

    Add files via upload

---
+++ problem5/js/index.js
@@ -0,0 +1,4 @@
+// write a java script program to get number from the user , and then display for the user in the console =>
+document.getElementById("mainHeading").innerHTML = "display the number in the console"
+var num = Number(window.prompt("please enter the number is = "))
+console.log("the number is = ",num)

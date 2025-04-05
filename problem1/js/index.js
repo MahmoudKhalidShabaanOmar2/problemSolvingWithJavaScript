58ffd86ac9bd8f8229e7ff5a524f52505135caea
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-05 02:01:35 +0200

    Add files via upload

---
+++ problem1/js/index.js
@@ -0,0 +1,3 @@
+// write a java script program to print hello world in the console =>
+document.getElementById("mainHeading").innerHTML = "Display Hello World! In The Console"
+console.log("Hello World! From Problem Solving With Java Script")

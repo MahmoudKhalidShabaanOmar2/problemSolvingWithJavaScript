c015c38ea8763cf516a5b9a81dd42c07fd2e90de
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-08 04:10:58 +0200

    Add files via upload

---
+++ problem16/js/index.js
@@ -0,0 +1,7 @@
+// write a java script program to get number from the user , and then get the square of the number => 
+document.getElementById("mainHeading").innerHTML = "Display The Number , And Then Get The Square Of The Number"
+var number = Number(window.prompt("Please Enter The Number Is = "))
+console.log("The Number Is = ",number)
+// squareOfNumber = number * number 
+squareOfNumber = number ** 2
+console.log("The Square Of The Number Is = ",squareOfNumber)

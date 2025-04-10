05477bf25f482a8c0c6143b9ff4d50570665302a
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-10 02:26:08 +0200

    Add files via upload

---
+++ problem22/js/index.js
@@ -0,0 +1,7 @@
+// write a java script program to get number from the user , then get the square of the number , and at the end get the number again by using the square root to get the number =>
+var number = Number(window.prompt("Please Enter The Number Is = "))
+console.log("The Number Is = ",number)
+var squareOfNumber = number * number 
+console.log("The Square Of The Number Is = ",squareOfNumber)
+var squareRootOfNumber = Math.sqrt(squareOfNumber)
+console.log("The Square Root Of The Number Is = ",squareRootOfNumber)

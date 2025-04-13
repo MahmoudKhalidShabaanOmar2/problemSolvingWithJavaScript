0880abcf188734b5eb741a6fe146d5bde5d18c4b
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-13 23:13:36 +0200

    Add files via upload

---
+++ problem29/js/index.js
@@ -0,0 +1,6 @@
+// write a java script program to get number of days to get number of hours =>
+document.getElementById("mainHeading").innerHTML = "Displaying Number Of Days And Then Numbers Of Hours"
+var numOfDays = Number(window.prompt("Please Enter The Number Of Days Is = "))
+console.log("The Number Of Days Is = ",numOfDays," Days")
+var numOfHours = numOfDays * 24 
+console.log("The Number Of Hours Is = ",numOfHours," Hours")

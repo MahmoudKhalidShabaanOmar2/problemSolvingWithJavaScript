f16f830a5a6877530c739e6520dc69b1bf56e973
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-04-13 22:57:56 +0200

    Add files via upload

---
+++ problem28/js/index.js
@@ -0,0 +1,6 @@
+// write a java script program to get numbers of years to get number of months => 
+document.getElementById("mainHeading").innerHTML = "Displaying Numbers Of Years , And Numbers Of Months"
+var numOfYears = Number(window.prompt("Please Enter The Number Of The Years Is = "))
+console.log("The Number Of Years Is ",numOfYears," Years")
+var numOfMonths = numOfYears * 12 
+console.log("The Number Of Months Is = ",numOfMonths," Months")

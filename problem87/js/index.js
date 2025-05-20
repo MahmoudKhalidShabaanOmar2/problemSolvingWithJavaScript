363f4c3a0a6a0487baf147eec173c6de26c54ae2
Mahmoud Khalid Shabaan Omar <mksomar200@gmail.com>
2025-05-20 23:37:49 +0300

    Add files via upload

---
+++ problem87/js/index.js
@@ -0,0 +1,8 @@
+// write a java script to create a function to create equation , and then return the answer of the eqution =>
+function solveEquation(exper) {
+    return eval(exper)
+}
+console.log("The Solve Of The Equation Is = ",solveEquation("1 + 1 + 1 + 1 + 1"))
+console.log("The Solve Of The Equation Is = ",solveEquation("7 * 4 + 2"))
+console.log("The Solve Of The Equation Is = ",solveEquation("7 * 4 - 2"))
+console.log("The Solve Of The Equation Is = ",solveEquation("4 * 9 - 45 / 9"))
